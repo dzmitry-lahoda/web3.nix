b00f8c5ec46bfa6ecb06f839aa68744fee72a887
Dzmitry Lahoda <dzmitry@lahoda.pro>
2023-01-04 10:10:53 +0000

    aws is used by crypto

--- flake.nix
+++ flake.nix
@@ -7,26 +7,42 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, nixos-generators, ... }: {
+  outputs = { self, nixpkgs, nixos-generators, ... }: 
+  let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in 
+  {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.virtualbox = nixos-generators.nixosGenerate {
       system = "x86_64-linux";
-    
+
       modules = [
-        ./modules/default.nix        
+        ./modules/virtualbox.nix
+        #"${toString modulesPath}/installer/virtualbox-demo.nix" 
       ];
+
       format = "virtualbox";
     };
+
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-    
+
     # shell with nixos-container
-    nixosConfgurations.myhost = nixpkgs.lib.nixosSystem {
+    nixosConfgurations.myhost = pkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [
-        ./modules/default.nix
+      modules = [ ./modules/virtualbox.nix ];
+    };
+
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = [
+        nixos-generators.packages.x86_64-linux.nixos-generate
+        pkgs.hello
+        pkgs.direnv
+        pkgs.nix-direnv
+        pkgs.awscli2
       ];
     };
+
+    crazy = "yes";
   };
 }
