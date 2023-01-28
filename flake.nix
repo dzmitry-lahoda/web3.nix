5b57664e9a6a640764884c64750229139af4b0a7
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-01-28 19:06:46 +0000

    terranix

--- flake.nix
+++ flake.nix
@@ -6,63 +6,74 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    terranix = {
+      url = "github:terranix/terranix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, nixos-generators, ... }: 
-  let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in 
-  {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.virtualbox = nixos-generators.nixosGenerate {
+  outputs = { self, nixpkgs, nixos-generators, terranix }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
       system = "x86_64-linux";
+      terrafromConfiguration = terranix.lib.terranixConfiguration {
+        inherit system;
+        modules = [ ./terranix/config.nix ];
+      };
 
-      modules = [
-        ./modules/virtualbox.nix
-        #"${toString modulesPath}/installer/virtualbox-demo.nix" 
-      ];
+    in {
 
-      format = "virtualbox";
+      packages.x86_64-linux = {
+
+        hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+        virtualbox = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+
+          modules = [
+            ./modules/virtualbox.nix
+            #"${toString modulesPath}/installer/virtualbox-demo.nix" 
+          ];
+
+          format = "virtualbox";
+        };
+
+        gce = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+
+          modules = [ ./modules/gce.nix ];
+
+          format = "gce";
+        };
+
+        default = self.packages.x86_64-linux.hello;
+      };
+
+      # shell with nixos-container
+      nixosConfgurations.myhost = pkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./modules/virtualbox.nix ];
+      };
+
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nixos-generators.packages.x86_64-linux.nixos-generate
+          hello
+          direnv
+          nix-direnv
+          awscli2
+          google-cloud-sdk
+          terraform
+        ];
+        shellHook = ''
+          (
+            cd gce-nixos/terraform/
+            terraform init --upgrade
+          )
+        '';
+      };
+
+      # hook to run `complete -C aws_completer aws`
+
+      crazy = "yes";
     };
-
-    packages.x86_64-linux.gce = nixos-generators.nixosGenerate {
-      system = "x86_64-linux";
-
-      modules = [
-        ./modules/gce.nix
-      ];
-
-      format = "gce";
-    };
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-    # shell with nixos-container
-    nixosConfgurations.myhost = pkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./modules/virtualbox.nix ];
-    };
-
-    devShell.x86_64-linux = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        nixos-generators.packages.x86_64-linux.nixos-generate
-        hello
-        direnv
-        nix-direnv
-        awscli2
-        google-cloud-sdk
-        terraform
-      ];
-      shellHook = ''
-      (
-        cd gce-nixos/terraform/
-        terraform init --upgrade
-      )
-      '';
-    };
-
-    # hook to run `complete -C aws_completer aws`
-
-    crazy = "yes";
-  };
 }
---
+++ terranix/config.nix
@@ -0,0 +1,6 @@
+{
+  resource.local_file_test_import = {
+    filename = "test_import.txt";
+    content = "Hello";
+  };
+}
